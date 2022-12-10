72d994972352c47f5b3f0ec46770a9e860a996d6
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-10 23:31:30 +0000

    v. 02 aula 03

---
+++ aula_03/src/app.js
@@ -0,0 +1,22 @@
+const MyNameApp = {
+    data(){
+        return{
+            name: "",
+            age: "",
+            input_name: "",
+            input_age: ""
+        }
+    },
+    methods:{
+        submitForm(e){
+            e.preventDefault();
+
+            console.log(this.input_name, this.input_age);
+
+            this.name = this.input_name;
+            this.age = this.input_age;
+        }
+    }
+}
+
+Vue.createApp(MyNameApp).mount("#app");
