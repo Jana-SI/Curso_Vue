d7ace61eb1a3f07ff3c3a2b8db58ad805839168b
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-11 23:21:31 +0000

    v. 03 aula 05

---
+++ aula_05/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <div class="container-fluid">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-6 col-sm-6 col-md-6 col-lg-6 text-center">
+        <div class="card text-bg-success text-center border border-white">
+          <div class="card-header border-bottom border-white">
+            <h2><strong>Curso Vue</strong></h2>
+          </div>
+          <div class="card-body">
+            <h3 class="card-title"><strong>Aula 05</strong></h3>
+            <h4 class="card-text border-top border-white">Olá mundo!</h4>
+            <Primeiro_Componente />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Primeiro_Componente from './components/Primeiro_Componente.vue';
+
+export default {
+  name: "App",
+  components: {
+    Primeiro_Componente
+  }
+};
+</script>
+
+<style>
+body {
+  background-image: url(./assets/fundo.jpg);
+}
+
+.row {
+  height: 100vh;
+}
+
+h2,
+h3 {
+  color: #37495f !important;
+}
+</style>
---
+++ aula_05/src/components/Primeiro_Componente.vue
@@ -0,0 +1,9 @@
+<template>
+    <h4 class="card-text border-top border-white">Olá Vue!</h4>
+</template>
+
+<script>
+export default {
+  name: 'PrimeiroComponente',
+}
+</script>
