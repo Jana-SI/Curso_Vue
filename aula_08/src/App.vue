5b43501a97a70c9a277ce73189346638eed6cb20
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-15 19:34:17 +0000

    v. 06 aula 08

---
+++ aula_08/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class="container-fluid">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-6 col-sm-6 col-md-6 col-lg-6 text-center">
+        <div class="card text-bg-success text-center border border-white">
+          <div class="card-header border-bottom border-white">
+            <h2><strong>Curso Vue</strong></h2>
+          </div>
+          <div class="card-body">
+            <h3 class="card-title"><strong>Aula 08</strong></h3>
+            <Pessoa />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Pessoa from './components/Pessoa.vue';
+
+export default {
+  name: "App",
+  components: {
+    Pessoa
+  }
+};
+</script>
+
+<style>
+body {
+  background-image: url(./assets/fundo.jpg);
+}
+
+.row {
+  height: 100vh;
+}
+
+h2,
+h3 {
+  color: #37495f !important;
+}
+
+.card-text{
+  padding: 5px;
+}
+</style>
---
+++ aula_08/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento!</p>
+        <p>Utilizio as seguintes tecnologias:</p>
+        <ul>
+            <li>Html5</li>
+            <li>CSS3</li>
+            <li>JS</li>
+            <li>Vue</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'info'
+}
+</script>
