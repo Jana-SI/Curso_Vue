2b5f37ddf346ba58306cc668980faa766b5e177e
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-16 22:31:49 +0000

    v. 13 aula 15

---
+++ aula_15/src/components/Multiplos_Eventos.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="row mb-3 align-items-center justify-content-center">
+        <div class="col-6">
+            <button class="btn" @click="primeiro(txt1, $event), segundo(txt2, $event), terceiro()">Ativar Múltiplos Eventos</button>
+        </div>
+        <div class="col-12 text-center">
+            <p>{{ mult_eventos }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Multiplos_Eventos',
+    data() {
+        return {
+            mult_eventos: "",
+            txt1: "Primeiro evento ativado!",
+            txt2: "Segundo evento ativado!"
+        }
+    },
+    methods: {
+        primeiro(txt, e) {
+            e.preventDefault();
+            this.mult_eventos = txt;
+        },
+        segundo(txt, e) {
+            e.preventDefault();
+           setTimeout(() => {
+            this.mult_eventos = txt;
+           }, 1000);
+        },
+        terceiro() {
+           setTimeout(() => {
+            this.mult_eventos = "";
+           }, 2000);
+           
+        }
+    }
+}
+</script>
+
+<style scoped>
+.btn {
+    background-color: #37495f !important;
+    color: white !important;
+    border: 2px solid white;
+}
+
+.btn:hover {
+    background-color: white !important;
+    color: #37495f !important;
+    border: 2px solid #37495f !important;
+}
+</style>
