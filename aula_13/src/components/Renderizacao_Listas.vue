8967949825310d58a970ce7db6f4344d1997f2d0
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-16 20:00:00 +0000

    v. 11 aula 13

---
+++ aula_13/src/components/Renderizacao_Listas.vue
@@ -0,0 +1,37 @@
+<template>
+    <p>Tecnologias Back-End:</p>
+    <ul>
+        <li v-for="(tecnologia, index) in backend_tecnologias" v-bind:key="index">{{ tecnologia }}</li>
+    </ul>
+    <p>Tecnologias Front-End:</p>
+    <ul>
+        <li v-for="tecnologia in frontend_tecnologias" v-bind:key="tecnologia.id">{{ tecnologia.language }}</li>
+    </ul>
+</template>
+<script>
+export default {
+    name: 'Renderizacao_Listas',
+    data() {
+        return {
+            backend_tecnologias: ['JS', 'PHP 8', 'Python'],
+            frontend_tecnologias: [
+                { id: 1, language: 'HTML5' },
+                { id: 2, language: 'CSS3' },
+                { id: 3, language: 'Vue 3' },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+}
+
+li {
+    margin-right: 10px;
+}
+</style>
