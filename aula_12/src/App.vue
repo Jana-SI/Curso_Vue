5e11c57639c92f627901e31359c1fc54a9be8fe0
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-16 18:46:37 +0000

    v. 10 aula 12

---
+++ aula_12/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+
+<Header />
+
+  <div class="container-fluid">
+
+    <div class="row align-items-center justify-content-center">
+      <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+        
+        <div class="card text-bg-success border border-white">
+          <div class="card-header text-center border-bottom border-white">
+            <h2><strong>Curso Vue</strong></h2>
+          </div>
+          <div class="card-body">
+            <h3 class="card-title text-center "><strong>Aula 12</strong></h3>
+            <Pessoa />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Pessoa from './components/Pessoa.vue';
+import Header from './components/Header.vue';
+
+export default {
+  name: "App",
+  components: {
+    Pessoa, Header
+  }
+};
+</script>
+
+<style>
+body {
+  background-image: url(./assets/fundo.jpg);
+}
+
+.row {
+  height: 90vh;
+}
+
+h2,
+h3 {
+  color: #37495f !important;
+}
+
+.card-text{
+  padding: 5px;
+}
+</style>
---
+++ aula_12/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Link 01</a></li>
+        <li><a href="#">Link 02</a></li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+ul{
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    background-color: #37495f;
+    padding: 10px;
+}
+
+li{
+    margin-right: 10px;
+}
+
+a{
+    color: white !important;
+    text-decoration: none;
+    transition: .5s;
+}
+
+a:hover{
+    color: green  !important;
+}
+</style>
