1d2bce9d1d8e45c36c1069858e2141f64259c213
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-20 22:57:37 +0000

    v. 15 aula 17

---
+++ aula_17/src/components/Props.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{ txt }}</p>
+    <p v-if="ShoworHide">Sou Boolean!</p>
+</template>
+
+<script>
+export default{
+    name: 'Props',
+    props: {
+        txt: String,
+        ShoworHide: Boolean
+    }
+}
+</script>
