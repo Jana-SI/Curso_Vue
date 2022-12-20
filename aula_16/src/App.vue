a2eb9caa6ea396801ba6625c0e162e22569e08b8
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-20 22:10:11 +0000

    v. 14 aula 16

---
+++ aula_16/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container-fluid">
+    <div id="rowP" class="row align-items-center justify-content-center">
+      <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="card text-bg-success border border-white">
+          <div class="card-header text-center border-bottom border-white">
+            <h2><strong>Curso Vue</strong></h2>
+          </div>
+          <div class="card-body">
+            <h3 class="card-title text-center "><strong>Aula 16</strong></h3>
+            <Reutilizacao />
+            <Reutilizacao />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Reutilizacao from './components/Reutilizacao.vue';
+
+export default {
+  name: "App",
+  components: {
+    Reutilizacao
+  }
+};
+</script>
+
+<style>
+body {
+  background-image: url(./assets/fundo.jpg);
+}
+
+#rowP {
+    height: 100vh;
+}
+
+h2,
+h3 {
+  color: #37495f !important;
+}
+
+.card-text {
+  padding: 5px;
+}
+</style>
---
+++ aula_16/src/components/Reutilizacao.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="row mb-3 align-items-center justify-content-center">
+        <div class="col-6 text-center">
+            <button class="btn" @click="ativar">clique aqui</button>
+        </div>
+        <div class="col-6 text-center">
+            <p v-show="ativou">Ativado!</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Reutilizacao',
+    data(){
+        return{
+            ativou: false
+        }
+    },
+    methods: {
+        ativar(){
+            this.ativou = !this.ativou
+        }
+    }
+}
+</script>
+
+<style scoped>
+.btn {
+    background-color: #37495f !important;
+    color: white !important;
+    border: 2px solid white;
+}
+
+.btn:hover {
+    background-color: white !important;
+    color: #37495f !important;
+    border: 2px solid #37495f !important;
+}
+</style>
