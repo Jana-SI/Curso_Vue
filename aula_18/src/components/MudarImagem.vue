8a69794d4d974639fbef5df49061a26021efc9f1
Janaina A. da Silva <62127906+Jana-SI@users.noreply.github.com>
2022-12-20 23:40:19 +0000

    v. 16 aula 18

---
+++ aula_18/src/components/MudarImagem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row mb-3 align-items-center justify-content-center">
+        <div class="col-12">
+            <button class="btn" @click="$emit('mudarImagem')">Trocar Imagem</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MudarImagem',
+    emits: ['mudarImagem']
+}
+</script>
+
+<style scoped>
+.btn {
+    background-color: #37495f !important;
+    color: white !important;
+    border: 2px solid white;
+}
+
+.btn:hover {
+    background-color: white !important;
+    color: #37495f !important;
+    border: 2px solid #37495f !important;
+}
+</style>
---
+++ aula_18/src/components/Picture.vue
@@ -0,0 +1,32 @@
+<template>
+    <img class="rounded rounded mx-auto d-block" :src="avatar" :alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem" @click="voltarImagem()"/>
+</template>
+
+<script>
+import MudarImagem from './MudarImagem.vue';
+
+export default {
+    name: "Picture",
+    components: { 
+        MudarImagem 
+    },
+    data() {
+        return {
+            avatar: "/img/avatar.png",
+            descricao: "img de avatar"
+        };
+    },
+    methods: {
+        trocarImagem(){
+            this.avatar="/img/avatar2.png"
+        },
+        voltarImagem() {
+           setTimeout(() => {
+            this.avatar="/img/avatar.png"
+           }, 2000);
+           
+        }
+    }
+}
+</script>
